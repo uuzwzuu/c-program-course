e6de2f0e5bd56431173414a8bc433bdfa647828a
William Zhong <william.zhong1109@gmail.com>
2025-12-31 11:42:06 -0500

    first draft version

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("Which Fibonacci number do you want to know?\n");
+
+    int n;
+    scanf("%d", &n);
+
+    printf("Your input is: %d\n", n);
+
+    printf("I will caculate the %d-th fibonacci number for you!\n", n);
+
+    return 0;
+}
+
+
+
+
+
