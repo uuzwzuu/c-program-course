12221568db6760b2bfa496c7404aaf6c3977a24e
William Zhong <william.zhong1109@gmail.com>
2025-12-31 12:25:38 -0500

    first version

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int fibonacci(int n)
+{
+    if (n==0) {return 0;}
+
+    int memory_size = (n+1) * 4; 
+    int *arr = (int *) malloc(memory_size);
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+    {    
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[n];
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +28,15 @@ int main(int argc, char *argv[])
 
     printf("Your input is: %d\n", n);
 
-    printf("I will caculate the %d-th fibonacci number for you!\n", n);
+    if (n < 0)
+    {
+        printf("Please input an interger >= 0.\n");
+        return 1;
+    }
+
+    int a = fibonacci(n);
+
+    printf("The %d-th fibonacci number is: %d\n", n, a);
 
     return 0;
 }
