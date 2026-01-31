b9c01d975c0581cf2ccab92c05bbe2efa6d3dd69
William Zhong <william.zhong1109@gmail.com>
2026-01-31 12:00:55 -0500

    insert a integer into a sorter array

--- insertion.c
+++ insertion.c
@@ -2,6 +2,33 @@
 #include<stdlib.h>
 #include<string.h>
 
+int array_insert(int * b, int len, int y)
+{
+    //insert y into array b[]
+
+    // 1. find the location (index i)
+    int j = 0;
+    for(int i=0; i<len; ++i)
+    {
+        if(b[i] > y)
+        {
+            j = i;
+            break;
+        }
+    } 
+
+    // 2. copy b[i],b[i+1],.... to the right
+    for(int i=len-2; i>=j; --i)
+    {
+        b[i+1] = b[i];
+    }
+
+    // 3. b[j] = y;
+    b[j] = y;
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     // arg 1 : comma separated sorted numbers like 1,3,5,7,9
@@ -28,7 +55,7 @@ int main(int argc, char *argv[])
     printf("Total Number: %d\n", comma_count);
 
     int length = comma_count +1;
-    int *a = (int *) malloc(length * 4);
+    int *a = (int *) malloc((length+1) * 4);
     
     a[0] = atoi(&argv[1][0]);
     int j = 1;    
@@ -42,10 +69,21 @@ int main(int argc, char *argv[])
         }
     }
 
-    for (int i=0; i<length; ++i)
+    for (int i=0; i<length+1; ++i)
     {
-        printf("%d\n", a[i]);
+        printf("%d ", a[i]);
     }
+    printf("\n");
+
+    int x = atoi(argv[2]);
+    array_insert(a, length+1, x);
+
+    for (int i=0; i<length+1; ++i)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
 
     free(a); a = NULL;
 
