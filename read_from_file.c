c0465b2269ae86424f982e1aeec8c8230017fae5
William Zhong <william.zhong1109@gmail.com>
2026-02-22 12:05:17 -0500

    Sort and print array

--- read_from_file.c
+++ read_from_file.c
@@ -2,6 +2,51 @@
 #include<stdlib.h>
 #include<string.h>
 
+int print_array(int *a, int len)
+{
+    for (int i=0; i<len; ++i)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int find_min_and_swap(int *a, int len)
+{
+    int j=0;
+    int m=a[0];
+    for(int i=0; i<len; ++i)
+    {
+        if (a[i]<m)
+        {
+            m=a[i];
+            j=i;
+        }
+    }
+   // printf("The min value is %d\n", m);
+   // printf("The index is %d\n", j);
+
+    int x=a[0];
+    a[0]=m;
+    a[j]=x;
+
+    //print_array(a, len);
+
+    return 0;
+}
+
+int sort_array(int *a, int len)
+{
+    for(int i=0; i<len-1; ++i)
+    {
+        find_min_and_swap(a+i, len-i);
+    }
+
+    return 0;
+
+}
 int print_file(char * filename)
 {
     FILE * fp = fopen(filename, "r");
@@ -13,20 +58,27 @@ int print_file(char * filename)
     }    
 
     char buf[255];
-
        
-    char * p=fgets(buf,sizeof(buf),fp);
+    char * p = fgets(buf,sizeof(buf),fp);
 
-    while(p!=NULL)
+    while(p!= NULL)
     {
-        char * token=strtok(buf, ",");
+        int a[10];
+        int len = 0;
+
+        char * token=strtok(buf, ",\n");
+
         while(token!=NULL)
         {
-            int val=atoi(token);
-            printf("%d ",val);
-            token=strtok(NULL,",");
+            a[len] = atoi(token);
+            len += 1;
+
+            token=strtok(NULL,",\n");
         }
         printf("\n");
+        printf("length:%d\n", len);
+        sort_array(a, len);
+        print_array(a, len);
         p=fgets(buf,sizeof(buf),fp);
     }
     printf("finished reading the file!\n");
