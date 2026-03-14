2e999e415a979c1b08747ad1da9399828461f3a8
William Zhong <william.zhong1109@gmail.com>
2026-03-14 12:03:10 -0400

    reverse

--- print_star.c
+++ print_star.c
@@ -1,18 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int print_triangle(int row)
+int print_space(int n)
 {
-    for (int j=0; j<row; ++j)
+    for (int i=0; i<n; ++i)
     {
-        for (int k=0; k<row-1-j; ++k)
-        {
-            printf(" ");
-        }
-        for (int i=0; i<2*j+1; ++i)
-        {
-            printf("*");
-        }
+        printf(" ");
+    }
+    return 0;
+}
+
+int print_char(int n, char c)
+{
+    for (int i=0; i<n; ++i)
+    {
+        printf("%c", c);
+    }
+    return 0;
+}
+
+int print_triangle(int row, char c)
+{
+    for (int i=0; i<row; ++i)
+    {
+        print_space(row-1-i);
+        print_char(2*i+1, c);
+        printf("\n");
+    }
+    return 0;
+}
+
+int print_reverse_triangle(int row, char c)
+{
+    for(int i=0; i<row; ++i)
+    {
+        print_space(i+1);
+        print_char(2*row-1-2*i, c);
         printf("\n");
     }
     return 0;
@@ -26,8 +49,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    char c = '*';
     int a = atoi(argv[1]);
-    print_triangle(a);
+    print_triangle(a, c);
+    print_reverse_triangle(a-1, c);
     return 0;
 }
 
