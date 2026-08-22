add7d480499de16703f3979b042cc303bb9535ee
William Zhong <william.zhong1109@gmail.com>
2026-08-22 12:03:26 -0400

    count.c

---
+++ count.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("Please load one file\n");
+        return 1;
+    }
+
+    char * filename = argv[1];
+    FILE * fp = fopen(filename, "r");
+
+    if(fp == NULL)
+    {
+        printf("File %s not found.\n", filename);
+        return 2;
+    }
+
+    char buf[100];
+    char * p = fgets(buf, 100, fp);
+
+    if(p != NULL)
+    {
+        printf("%s\n", buf);
+    }
+    else
+    {
+        printf("reading error.\n");
+        return 3;
+    }
+
+    char delims[] = ",";
+
+    int x = 0;
+
+    char * token = strtok(buf, delims);
+
+    while(token != NULL)
+    {
+        printf("%s\n", token);
+        x = x + 1;
+        token = strtok(NULL, delims);
+    }
+
+    printf("%d\n", x);
+
+    fclose(fp);
+
+    return 0;
+}
